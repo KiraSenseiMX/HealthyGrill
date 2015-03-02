9c0c67c137c8d72276b9b494a86277e4c8c8a9d7
OsamuKT <oskatagiri@gmail.com>
2015-03-02 17:54:34 -0600

    ALL recipies added to the dataBase

--- app/src/main/java/mx/itesm/osamukt/healthygrill/MainActivity.java
+++ app/src/main/java/mx/itesm/osamukt/healthygrill/MainActivity.java
@@ -31,26 +31,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         /*ELIMINAR O INSERTAR RECETAS A LA BASE DE DATOS
         * manager.eliminar(StrReceta);
         * manager.insertar(StrReceta,StrFunción,StrTemperatura,StrUtencilio,StrIngredientes,StrProcedimiento); */
-        manager.eliminar("Salmón con Salsa de Vino Blanco");
-        manager.insertar("Salmón con Salsa de Vino Blanco",
-                //"Carnes y Aves",
-                "Bake",
-                "450 °F",
-                "Charola asador",
-                "700 g de filete de salmón, corte grueso\n" +
-                        "Sal y pimienta\n" +
-                        "1 cucharada de mantequilla\n" +
-                        "2 cucharaditas de harina de trigo\n" +
-                        "1 taza de vino blanco seco\n" +
-                        "1 cucharada de cebollín fresco picado\n",
-                "Precalentar el horno. Colocar el salmón en la charola y sazonar con sal. Hornear por 15 minutos o hasta que adquiera un color opaco. Para preparar la salsa, derretir mantequilla a fuego medio, agregar harina y cocinar por 1 minuto, mezclando continuamente. Agregar el vino y cocinar hasta la ebullición, reducir el fuego al mínimo y cocinar hasta que se evapore la mitad del líquido (8 - 10 minutos). Agregar el cebollín y sazonar con sal y pimienta. Servir el salmón bañado en la salsa de vino blanco.");
         cursor = manager.cargarCursorRecipes();
         String[] RecipeNamesArray = getResources().getStringArray(R.array.recipies_array);
         ArrayAdapter<String> RecipeNamesAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,RecipeNamesArray);
         tv.setThreshold(1);
         tv.setAdapter(RecipeNamesAdapter);
         /*TEST*/
-        String[] from = new String[]{manager.CN_RECIPENAME,manager.CN_PROCESS};
+        String[] from = new String[]{manager.CN_RECIPENAME,manager.CN_INGREDIENTS};
         int[] to = new int[]{android.R.id.text1,android.R.id.text2};
         adapter = new SimpleCursorAdapter(this,android.R.layout.two_line_list_item,cursor,from,to,0);
         lista.setAdapter(adapter);
